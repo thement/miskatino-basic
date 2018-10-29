7b2b9b0153c3be50d3dfbeb06dbb2f5b0c8a2f02
rodion <rodiongork@github.com>
2018-10-29 21:05:55 +0300

    JS build file updated

--- js/build.sh
+++ js/build.sh
@@ -1,2 +1,5 @@
-em++ -Wall -Wno-write-strings -Wno-char-subscripts -o tinybas.js -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' main_js.c ../core/*.c
-#docker run --rm -v $(pwd):/src trzeci/emscripten em++ -s WASM=0
+docker run --rm -v $(pwd)/..:/src -w /src/js trzeci/emscripten em++ \
+    -Wall -Wno-write-strings -Wno-char-subscripts -o tinybas.js \
+    -s WASM=0 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
+    main_js.c ../core/*.c
+
